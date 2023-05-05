c40e6a3573d67b36d571ed05d54794e72274ac07
Kenpo <kenpoerickson@gmail.com>
2023-05-05 02:45:15 -0500

    vercel7 Swagger

--- src/main.ts
+++ src/main.ts
@@ -31,7 +31,14 @@ async function bootstrap() {
 		.setVersion('1.0')
 		.build();
 	const document = SwaggerModule.createDocument(app, config);
-	SwaggerModule.setup('docs', app, document);
+	SwaggerModule.setup('docs', app, document, {
+		customCssUrl:
+			'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+		customJs: [
+			'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+			'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+		],
+	});
 
 	app.enableCors();
 	await app.listen(process.env.PORT || 3000, '0.0.0.0');
