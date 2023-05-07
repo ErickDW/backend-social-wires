2f574c1547686072ae7ec7e2126773ce3ca9d892
Kenpo <kenpoerickson@gmail.com>
2023-05-07 05:01:17 -0500

    vercel

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,6 @@ async function bootstrap() {
 	const app = await NestFactory.create<NestFastifyApplication>(
 		AppModule,
 		new FastifyAdapter(),
-		{ cors: true },
 	);
 	await app.register(fastifyCookie, {
 		secret: 'my-secret', // for cookies signature
